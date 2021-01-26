743fd8c067e0359d8bb1e86c549cdd005a8878b7
floss-order <flossOrder@gmail.com>
2021-01-26 13:43:18 +0000

    Updated schema

--- src/schema.js
+++ src/schema.js
@@ -9,7 +9,12 @@ const errorMessage = 'Введено некорректное значение'
 const schema = yup.object().shape({
   name: yup.string().required(requiredMessage).matches(nameRegExp, errorMessage),
   email: yup.string().required(requiredMessage).email(errorMessage),
-  phone: yup.string().required(requiredMessage).matches(phoneRegExp, errorMessage)
+  phone: yup.string().required(requiredMessage).matches(phoneRegExp, errorMessage),
+  policyCheckbox: yup.bool().oneOf([true]).required(),
+  language: yup.object().shape({
+    value: yup.string(),
+    label: yup.string(),
+  }).nullable().required()
 }) 
 
 export default schema
